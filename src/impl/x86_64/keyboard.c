804c59bf89bf258d41f3c4ae2553936829c686bb
pauytrh2 <pauytrh@gmail.com>
2025-04-23 17:57:33 +0300

    Add keyboard input

--- src/impl/x86_64/keyboard.c
+++ src/impl/x86_64/keyboard.c
@@ -3,17 +3,26 @@
 #include "keyboard.h"
 #include "print.h"
 
+static const char scancode_to_ascii[] = {
+    0,  27, '1','2','3','4','5','6','7','8','9','0','-','=', '\b', // 0x00 - 0x0E
+    '\t','q','w','e','r','t','y','u','i','o','p','[',']','\n',     // 0x0F - 0x1C
+    0,   'a','s','d','f','g','h','j','k','l',';','\'','`',         // 0x1D - 0x29
+    0,  '\\','z','x','c','v','b','n','m',',','.','/',              // 0x2A - 0x35
+    0,   '*', 0,  ' '                                              // 0x36 - 0x39
+};
+
 void keyboard_loop() {
     while (1) {
         if (inb(0x64) & 1) { // if there is data in buffer
             uint8_t scancode = inb(0x60);
-            print_str("Scancode: ");
-            char hex[3];
-            hex[0] = "0123456789ABCDEF"[scancode >> 4];
-            hex[1] = "0123456789ABCDEF"[scancode & 0x0F];
-            hex[2] = '\0';
-            print_str(hex);
-            print_str("\n");
+
+            if (scancode < sizeof(scancode_to_ascii)) {
+                char c = scancode_to_ascii[scancode];
+                if (c) {
+                    char str[2] = {c, '\0'};
+                    print_str(str);
+                }
+            }
         }
     }
 }
