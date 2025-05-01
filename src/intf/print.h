a91c542ead14a3e18e980d93d91d009a989e1706
pauytrh2 <pauytrh@gmail.com>
2025-05-01 21:18:16 +0300

    Patch lil err by adding const type

--- src/intf/print.h
+++ src/intf/print.h
@@ -26,5 +26,5 @@ enum {
 
 void print_clear();
 void print_char(char character);
-void print_str(char* string);
+void print_str(const char* string);
 void print_set_color(uint8_t foreground, uint8_t background);
